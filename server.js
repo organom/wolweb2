a946a43d74dfddef784a0382542904cd4c9aeb6e
Ricardo Gomes <ricardo.gomes@brainlab.net>
2017-10-02 13:44:47 +0200

    Improved the add computer flow and homepage display.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express')
   , logger = require('morgan')
   , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+  , homepage = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+  , addcomputer = require('jade').compileFile(__dirname + '/source/templates/addcomputer.jade')
   , fs = require('fs')
   , wol = require('node-wol')
 
@@ -10,7 +11,7 @@ app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
 function getComputers() {
-  var file = "static/computers.json";
+  var file = __dirname + "/static/computers.json";
   var computers = "";
   if (fs.existsSync(file)) {
     var data = fs.readFileSync(file, 'utf8');
@@ -23,18 +24,59 @@ function getComputers() {
 
 app.get('/', function (req, res, next) {
   try {
-    var html = template({ computers: getComputers() })
+    var html = homepage({ computers: getComputers() })
     res.send(html)
-  } catch (e) {
-    next(e)
+  } 
+  catch (ex) {
+    next(ex)
+  }
+})
+
+app.get('/add', function (req, res, next) {
+  try
+  {
+    var html = addcomputer()
+    res.send(html)
+  }
+  catch(ex) {
+    next(ex)
+  }
+})
+
+app.get('/addcomputer', function (req, res, next) {
+  try
+  {
+    var file = __dirname + "/static/computers.json";
+    if (fs.existsSync(file)) {
+      var computers = getComputers();
+      computers[req.params];
+      fs.writeFileSync(file, computers, 'utf8');
+    }
+  }
+  catch(ex) {
+    next(ex)
+  }
+})
+
+app.get('/wake', function (req, res, next) {
+  try
+  { 
+    
+  }
+  catch(ex) {
+    next(ex)
   }
 })
 
 
-app.get('/add', function (req, res, next) {
-})
-
 app.get('/wakeall', function (req, res, next) {
+  try
+  {
+
+  }
+  catch(ex) {
+    next(ex)
+  }
 })
 
 app.listen(process.env.PORT || 3002, function () {
